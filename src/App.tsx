0461130de9785581a2247a9f6af5a0ba10f7b0e3
Denis Yunyaev <music_is_my_extazy@mail.ru>
2023-05-20 23:41:39 +0300

    6. Routing. Code spliting. Lazy suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import { Counter } from './components/Counter';
+import React, { Suspense } from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+
+// import AboutPage from './pages/AboutPage/AboutPage';
+// import MainPage from './pages/MainPage/MainPage';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 import './index.scss';
 
 const App = () => {
   return (
     <div className="app">
-      <span>qwewer</span>
-      <Counter />
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
---
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -0,0 +1,8 @@
+import { lazy } from 'react';
+
+export const AboutPageAsync = lazy(
+  () =>
+    new Promise((resolve) => {
+      setTimeout(() => resolve(import('./AboutPage') as never), 1000);
+    }),
+);
