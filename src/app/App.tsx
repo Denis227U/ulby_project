8faffbb550bdd7763ca64cdf41daeb144ca281a3
Denis Yunyaev <music_is_my_extazy@mail.ru>
2023-05-22 02:58:34 +0300

    11. AppRouter. Конфиг для роутера

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,7 @@ import { Route, Routes, Link } from 'react-router-dom';
 
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { AppRouter } from 'app/providers/router';
 
 import './styles/index.scss';
 
@@ -16,12 +15,7 @@ const App = () => {
       <button onClick={toggleTheme}>TOGGLE THEME</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
