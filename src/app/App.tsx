6cb2b8ead3d6e1ab153b85aba44342a2d605de90
Denis Yunyaev <music_is_my_extazy@mail.ru>
2023-05-22 02:35:44 +0300

    10. Архитектура FSD. Начинаем внедрять

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,12 @@
 import React, { Suspense } from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 
-// import AboutPage from './pages/AboutPage/AboutPage';
-// import MainPage from './pages/MainPage/MainPage';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { useTheme } from './theme/useTheme';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 import './styles/index.scss';
-import { classNames } from './helpers/classNames/classNames';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -20,8 +18,8 @@ const App = () => {
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/about" element={<AboutPageAsync />} />
-          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/" element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
---
+++ src/app/providers/ThemeProvider/index.ts
@@ -0,0 +1,5 @@
+// export { ThemeProvider as default } from './ui/ThemeProvider';
+import ThemeProvider from './ui/ThemeProvider';
+import { useTheme } from './lib/useTheme';
+
+export { ThemeProvider, useTheme };
