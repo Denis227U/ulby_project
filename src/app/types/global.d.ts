5d0b51720f2e6e71cce7833a3f77c1fffbfd6006
Denis Yunyaev <music_is_my_extazy@mail.ru>
2023-05-25 22:11:20 +0300

    25. Настройка Storybook с декораторами. Пишем stories для компонентов

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -6,12 +6,20 @@ declare module '*.scss' {
   export = classNames;
 }
 
+declare module '*.svg' {
+  import React = require('react');
+
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
-declare module '*.svg' {
-  const content: any;
-  export default content;
-}
+// declare module '*.svg' {
+//   const content: any;
+//   export default content;
+// }
 
 declare const __IS_DEV__: boolean;
--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
-const AboutPage = () => {
+const AboutPage: FC = () => {
   const [t] = useTranslation('about'); // в хук передаем название файла в locales
 
   return <div>{t('О сайте')}</div>;
